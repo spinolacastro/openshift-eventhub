1563c7017ab8e5cd88b134362ba2939ab81f7493
Diego Castro <spinolacastro@gmail.com>
2016-09-26 18:26:30 -0300

    fix

--- send.py
+++ send.py
@@ -9,20 +9,21 @@ sbs = ServiceBusService('getupevents',
                         shared_access_key_name=key_name,
                         shared_access_key_value=key_value)
 
-def send(namespace, id, time, reason, message):
+def send(namespace, id, time, reason, message, count):
     message = {
             "time": time,
             "namespace": namespace,
             "id": id,
             "reason": reason,
-            "message": message
+            "message": message,
+            "count": count
         }
     print(json.dumps(message))
     return sbs.send_event('toanalytics', json.dumps(message))
 
 try:
-    namespace, id, time, reason, message = sys.argv[1:]
-    print send(namespace, id, time, reason, message)
+    namespace, id, time, reason, message, count = sys.argv[1:]
+    print send(namespace, id, time, reason, message, count)
 except ValueError:
-    print "Use: {} [lasttimestamp] [reason]".format(argv[0])
+    print "Use: {} [reason] [message] [count]".format(argv[0])
     sys.exit(0)
